c4bfc2f701c8eb85f0b463a40170b297d3343653
balkrishantechsumsolution <balkrishan.c@techsumsolution.com>
2023-03-29 12:37:43 +0530

    Support part completed

--- BugTrackingSys/Areas/Support/Models/UsersRolesViewModel.cs
+++ BugTrackingSys/Areas/Support/Models/UsersRolesViewModel.cs
@@ -1,6 +1,14 @@
 ﻿using BugTrackingSys.Models;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+using FluentValidation.TestHelper;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 
 namespace BugTrackingSys.Areas.Support.Models
 {
@@ -19,13 +27,39 @@ namespace BugTrackingSys.Areas.Support.Models
         public RoleModel role { get; set; }
         public UsersModel user { get; set; }
 
+        public Tasks tasks { get; set; }
+
+        public TasksRemark tasksRemark { get; set; }
 
 
+        [NotMapped]
+        public FileViewModel fileViewModel { get; set; }
+        [NotMapped]
+        public List<FileViewModel> fileAttach { get; set; }
+        [NotMapped]
         public List<User_RolesModel> User_RolesModelList { get; set; }
+        [NotMapped]
         public List<UsersModel> UsersList { get; set; }
+        [NotMapped]
         public List<RoleModel> RoleList { get; set; }
-        public List<ProjectViewModel> ProjectList { get; set; }
 
+        [NotMapped]
+        public List<Tasks> taskList { get; set; }
+
+        [NotMapped]
+        public List<TasksRemark> taskRemarkList { get; set; }
+
+        [NotMapped]
+        public List<SelectListItem> ProjectList { get; set; }
+
+        [NotMapped]
+        public CalendarEvent cal { get; set; }
+
+        [NotMapped]
+        public List<IFormFile> files { get; set; }
+
+        [NotMapped]
+        public List<Tasks> CalendarEventList { get; set; }
 
         [NotMapped]
         public IList<SelectListItem> RoleMainList { get; set; }
@@ -33,19 +67,39 @@ namespace BugTrackingSys.Areas.Support.Models
         [NotMapped]
         public IList<SelectListItem> UserMainList { get; set; }
 
+        [NotMapped]
+        public IList<SelectListItem> OwnerMainList { get; set; }
+
+        [NotMapped]
+        public IList<SelectListItem> AssigneeMainList { get; set; }
+
+        [NotMapped]
+        public List<SelectListItem> TaskList { get; set; }
+
+        [NotMapped]
+        public List<SelectListItem> PriorityList { get; set; }
+
 
 
         public UsersRolesViewModel()
         {
-            RoleList = new List<RoleModel>();
-            User_RolesModelList = new List<User_RolesModel>();
-            UsersList = new List<UsersModel>();
-            ProjectList = new List<ProjectViewModel>();
+
+
+
         }
+
+        //    public UsersRolesViewModel()
+        //{
+        //    RoleList = new List<RoleModel>();
+        //    User_RolesModelList = new List<User_RolesModel>();
+        //    UsersList = new List<UsersModel>();
+        //    ProjectList = new List<ProjectViewModel>();
+
+        //}
     }
     public class ProjectViewModel
     {
-        public string Id { get; set; }
+        public string id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
@@ -57,4 +111,84 @@ namespace BugTrackingSys.Areas.Support.Models
         public string Isactive { get; set; }
     }
 
+    public class CalendarEvent
+    {
+        public string id { get; set; }
+        public string start { get; set; }
+        public string end { get; set; }
+        public string text { get; set; }
+        public string? color { get; set; }
+    }
+
+    public class FileViewModel
+    {
+        public int FileID { get; set; }
+        public string TaskID { get; set; }
+        public string FileName { get; set; }
+        public string FileData { get; set; }
+        public string FileType { get; set; }
+        public string IsActive { get; set; }
+    }
+
+    public class TasksValidator : AbstractValidator<UsersRolesViewModel>
+    {
+
+
+        public TasksValidator()
+        {
+            RuleFor(usersRolesViewModel => usersRolesViewModel.tasks.TaskName)
+           .NotEmpty().NotNull()
+            .WithMessage("You should select a Task Name");
+            RuleFor(usersRolesViewModel => usersRolesViewModel.tasks.TaskName).Must(Task_Length).WithMessage("Task name Length must be equal   or greater than 3");
+
+            RuleFor(usersRolesViewModel => usersRolesViewModel.tasks.Startdate)
+           .NotEmpty()
+            .WithMessage("You should select a start date");
+            RuleFor(usersRolesViewModel => usersRolesViewModel.tasks.Enddate)
+          .NotEmpty()
+           .WithMessage("You should select a end date");
+            RuleFor(usersRolesViewModel => usersRolesViewModel.tasks.Startdate).LessThanOrEqualTo(usersRolesViewModel => usersRolesViewModel.tasks.Enddate).WithMessage("Start Date must be greater than End Date");
+
+            RuleFor(usersRolesViewModel => usersRolesViewModel.tasks.TaskStatus)
+            .NotEmpty()
+            .WithMessage("You should select a Task Status");
+            RuleForEach(usersRolesViewModel => usersRolesViewModel.files).SetValidator(new FileValidator());
+
+
+        }
+        private bool Task_Length(string task_)
+        {
+
+            if (task_ != null)
+            {
+                if (task_.Length < 3)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+
+    public class FileValidator : AbstractValidator<IFormFile>
+    {
+        public int FileSize { get; set; } = 1 * 1024 * 1024 * 1024;
+        public FileValidator()
+        {
+            RuleFor(x => x.Length)
+            .LessThanOrEqualTo(FileSize)
+            .WithMessage("Maximum allowed file size is 5 MB");
+
+        }
+
+    }
 }
