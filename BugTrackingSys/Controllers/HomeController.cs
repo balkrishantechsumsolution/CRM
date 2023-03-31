249eeffe313801c811978cd987670d43db4f50da
balkrishantechsumsolution <balkrishan.c@techsumsolution.com>
2023-03-31 12:53:23 +0530

    31-03-2023

--- BugTrackingSys/Controllers/HomeController.cs
+++ BugTrackingSys/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using BugTrackingSys.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Mail;
+using System.Net;
 
 namespace BugTrackingSys.Controllers
 {
@@ -27,6 +29,48 @@ namespace BugTrackingSys.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult EmailIndex()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EmailIndex(EmailModel model)
+        {
+            using (MailMessage mm = new MailMessage(model.Email, model.To))
+            {
+                mm.Subject = model.Subject;
+                mm.Body = model.Body;
+                if (model.Attachment.Length > 0)
+                {
+                    string fileName = Path.GetFileName(model.Attachment.FileName);
+                    mm.Attachments.Add(new Attachment(model.Attachment.OpenReadStream(), fileName));
+                }
+                mm.IsBodyHtml = false;
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    //smtp.Host = "smtp.gmail.com";
+                    //smtp.EnableSsl = true;
+                    //NetworkCredential NetworkCred = new NetworkCredential(model.Email, model.Password);                   
+                    //smtp.UseDefaultCredentials = false;
+                    //smtp.Credentials = NetworkCred;
+                    //smtp.Port = 587;
+                    //smtp.Send(mm);
+                    //ViewBag.Message = "Email sent.";
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential(model.Email, model.Password);
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Send(mm);
+                    ViewBag.Message = "Email sent.";
+                }
+            }
+
+            return View("EmailIndex");
         }
     }
 }
